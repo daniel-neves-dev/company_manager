e25961c2303c4c240d6e4a39d2ef6485324d8d40
daniel-dev-neves <daniel.oliveira.au@hotmail.com>
2024-04-06 18:07:23 -0300

    feature: employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+  enum department: {human_resource:0, information_technology:1, finance:2, sales:3, operations:4}
+  enum role: {manager:0, senior_specialist:1, analyst:2, coordinator:3, engineer:4}
+  enum status: {active:0, on_leave:1, resigned:2, retired:3, under_review:4}
 end
