0583e04e07e1f44c89ca1ae6db2f2f499660d77b
daniel-dev-neves <daniel.oliveira.au@hotmail.com>
2024-04-07 11:24:27 -0300

    test: employee

--- spec/features/employee_spec.rb
+++ spec/features/employee_spec.rb
@@ -57,13 +57,12 @@ RSpec.describe 'Employee navigation', type: :feature do
 
       click_button('Add Employee')
 
-
       expect(page).to have_content(first_name)
       expect(page).to have_content(last_name)
       expect(page).to have_content(employee_id)
-      expect(page).to have_content('finance')
-      expect(page).to have_content('coordinator')
-      expect(page).to have_content('active')
+      expect(page).to have_content(department)
+      expect(page).to have_content(role)
+      expect(page).to have_content(status)
     end
   end
 
@@ -86,9 +85,9 @@ RSpec.describe 'Employee navigation', type: :feature do
       expect(page).to have_content('John')
       expect(page).to have_content('Doe')
       expect(page).to have_content('EMP001')
-      expect(page).to have_content('finance')
-      expect(page).to have_content('analyst')
-      expect(page).to have_content('active')
+      expect(page).to have_content('Finance')
+      expect(page).to have_content('Analyst')
+      expect(page).to have_content('Active')
     end
   end
 
@@ -111,6 +110,34 @@ RSpec.describe 'Employee navigation', type: :feature do
       expect(page).to have_field('employee_status')
 
       expect(page).to have_selector('input[type="submit"]')
+    end
+
+    it 'creates an employee from the new form page' do
+      first_name = "John"
+      last_name = "Joe"
+      employee_id = "EMP001"
+      department = "Finance"
+      role = "Coordinator"
+      status = "Active"
+
+      visit edit_employee_path(employee)
+
+      fill_in 'employee[first_name]', with: first_name
+      fill_in 'employee[last_name]', with: last_name
+      fill_in 'employee[employee_id]', with: employee_id
+
+      select department, from: 'employee[department]'
+      select role, from: 'employee[role]'
+      select status, from: 'employee[status]'
+
+      click_button('Save Changes')
+
+      expect(page).to have_content(first_name)
+      expect(page).to have_content(last_name)
+      expect(page).to have_content(employee_id)
+      expect(page).to have_content(department)
+      expect(page).to have_content(role)
+      expect(page).to have_content(status)
     end
   end
 end
