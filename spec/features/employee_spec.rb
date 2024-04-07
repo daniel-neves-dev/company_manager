8357bd2d9c81ea99ed230814b2311a7d94a2f0a1
daniel-dev-neves <daniel.oliveira.au@hotmail.com>
2024-04-07 09:45:46 -0300

    test: employee

--- spec/features/employee_spec.rb
+++ spec/features/employee_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe 'Employee navigation', type: :feature do
       expect(page).to have_content("Employees")
     end
   end
+
+  it 'show a list of employees names', type: :feature, js: true do
+    FactoryBot.create(:employee, first_name: 'John', last_name: 'Doe', employee_id: 'EMP001')
+    FactoryBot.create(:employee, first_name: 'Jane', last_name: 'Peg', employee_id: 'EMP002')
+
+    visit employees_path
+
+    expect(page).to have_content('John Doe')
+    expect(page).to have_content('Jane Peg')
+  end
+
+  describe 'Employee show page', type: :feature, js: true do
+    it 'show a employees details',type: :feature do
+      employee = FactoryBot.create(:employee, first_name: 'John', last_name: 'Doe', employee_id: 'EMP001', department:'finance', role:'analyst', status:'active')
+
+      visit employee_path(employee)
+
+      expect(page).to have_content('John')
+      expect(page).to have_content('Doe')
+      expect(page).to have_content('EMP001')
+      expect(page).to have_content('finance')
+      expect(page).to have_content('analyst')
+      expect(page).to have_content('active')
+    end
+  end
 end
