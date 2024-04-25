da38c53f0ff6d42c228a7418c48d0d324dc7d73b
daniel-dev-neves <daniel.oliveira.au@hotmail.com>
2024-04-25 11:44:24 -0300

    style: alert

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,25 +1,32 @@
-import { Controller } from "@hotwired/stimulus";
+import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["alert"]; // Define a target for the alert
+    static targets = ["alert", "photoCard"];
 
-  connect() {
-    // Set a timeout to fade out the alert
-    setTimeout(() => {
-      this.fadeOut();
-    }, 1000); // Delay before starting fade out, adjust as necessary
-  }
-
-  fadeOut() {
-    const alertElement = this.alertTarget;
-    if (alertElement) {
-      alertElement.style.transition = 'opacity 1s ease-in-out';
-      alertElement.style.opacity = '0';
-
-      // Remove the element from the DOM after the transition
-      setTimeout(() => {
-        alertElement.remove();
-      }, 1000); // Duration of the fade out, must match the CSS transition
+    connect() {
+        this.photoCardTarget.classList.add('mt-[size_of_alert]');
+        setTimeout(() => {
+            this.fadeOut();
+        }, 2000);
     }
-  }
+
+    fadeOut() {
+        if (this.hasAlertTarget) {
+            const alertElement = this.alertTarget;
+            alertElement.style.transition = 'opacity 1s ease-in-out';
+            alertElement.style.opacity = '0';
+
+            setTimeout(() => {
+                if (this.hasAlertTarget) {
+                    alertElement.remove();
+
+                    // Apply the margin-top transition to the photo card
+                    if (this.hasPhotoCardTarget) {
+                        this.photoCardTarget.style.transition = 'margin-top 1s ease-in-out';
+                        this.photoCardTarget.style.marginTop = '36px';
+                    }
+                }
+            }, 1000);
+        }
+    }
 }
