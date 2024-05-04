f70136b1bba3565d68fc3ea45bd806211be1f5e3
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2024-05-04 11:08:21 -0300

    style: navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def navbar_link(text, path)
     link_to I18n.t(text), path,
-            class: "py-2 px-3 text-white bg-blue-700 rounded hover:bg-blue-800 md:bg-transparent md:text-blue-300 md:hover:text-blue-500 md:p-0", aria: { current: "page" }
+            class: "py-2 px-3 text-white md:bg-transparent text-blue-300 hover:text-blue-500 md:p-0",
+            aria: { current: "page" }
   end
 end
